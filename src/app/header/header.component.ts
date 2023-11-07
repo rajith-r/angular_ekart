ce7d9bed9e81a3101ebee5f86842981c5c08ea59
rajith-r <rajithrekenson@gmail.com>
2023-11-07 16:20:02 -0500

    Splitting header UI

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { TopMenuComponent } from "./top-menu/top-menu.component";
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.component.css'],
+})
+export class HeaderComponent{
+
+}
